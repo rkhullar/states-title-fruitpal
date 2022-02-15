4379969cd23b94a5ade8879f4bf2c3d00fdb2042
Rajan Khullar <rkhullar03@gmail.com>
2022-02-15 10:25:06 -0500

    save

--- backend/fastapi/api/v1/router.py
+++ backend/fastapi/api/v1/router.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from .routes import default
 
 router = APIRouter()
-router.include_router(default.router, prefix='/', tags=[])
+router.include_router(default.router, prefix='', tags=[])
