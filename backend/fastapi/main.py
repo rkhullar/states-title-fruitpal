935e008ebf69ca90a1139bd8f6e4cb7b25b0746c
Rajan Khullar <rkhullar03@gmail.com>
2022-02-15 10:12:09 -0500

    save

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,41 +1,10 @@
-from decimal import Decimal
-
 import uvicorn
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
 
-from model import Vendor as VendorInDB
-from schema import Vendor
+from api.core.config import Settings
+from api.core.factory import create_app
 
-app = FastAPI()
-
-
-vendor_database = [
-   VendorInDB(country='MX', commodity='mango', variable_overhead=Decimal('1.24')),
-   VendorInDB(country='BZ', commodity='mango', variable_overhead=Decimal('1.42')),
-]
-
-
-@app.get('/vendors', response_model=list[Vendor])
-async def list_vendors():
-    def iter_vendors():
-        for item in vendor_database:
-            yield jsonable_encoder(Vendor(**item.dict()))
-    return list(iter_vendors())
-
-
-@app.post('/vendors', response_model=Vendor)
-async def create_vendor(data: Vendor) -> Vendor:
-    return jsonable_encoder(data)
-
-
-@app.get('/estimate')
-async def estimate(unit_price: int, volume: int):
-    return dict(example=[
-        {'country': 'MX', 'price': vendor_database[0].calculate_purchase_cost(unit_price, volume)},
-        {'country': 'BZ', 'price': vendor_database[1].calculate_purchase_cost(unit_price, volume)}
-    ])
-
+settings = Settings()
+app = create_app(settings)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('main:app', host=settings.service_host, port=settings.service_port, reload=settings.debug)
